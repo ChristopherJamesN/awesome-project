bb940c4afc4090298b06531391dde7830bf33223
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-10 11:10:15 -0600

    Initial commit.

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import Bananas from './Bananas'
+import PizzaTranslator from './PizzaTranslator'
+import ButtonBasics from './ButtonBasics'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.bigblue}>Shake your phone to open the developer menu.</Text>
+          <Bananas />
+          <PizzaTranslator />
+          <ButtonBasics />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+    width: 50,
+    height: 50,
+  },
+});
